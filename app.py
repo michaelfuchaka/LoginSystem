b2460b7f2e0ba8f727ba8de354a62ec144fabba8
michaelfuchaka <fuchakamichael06@gmail.com>
2025-09-21 21:37:19 +0300

    Implement user authentication with login and logout routes; add test user creation and session checking functionality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import session       
 from models import db, User, init_db
+from flask import request, jsonify
 
 app = Flask(__name__) 
 
@@ -13,22 +14,55 @@ init_db(app)
 
 app.secret_key = "12345678"
       
-@app.route('/')
-def greeting():    
-    return "Hello, World!"
+@app.route('/login', methods=['POST'])
+def login():    
+    data=request.get_json()
+    username=data.get("username")
 
-
-@app.route('/test-session')
-def test_session():
-    if "user_id" not in session:
-        session["user_id"] = 42
-        return "Session set!"
+    if username is None:
+        return "Username is required", 400
+    
+    user = User.query.filter_by(username=username).first()
+    if user :
+        session["user_id"] = user.id
+        return jsonify({"message": "Login successful", "user": username})
     else:
-        session.pop("user_id", None)
-        return  "Session cleared!"
+        return "Error: User not found", 404
 
-                                                                                                                                        
 
+    
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    session.clear()
+    return jsonify ({'message': "Logged out successfully"}), 200
+
+@app.route('/create-test-user')
+def test_user():
+    """Route to insert some test users into the database."""
+    # Check if users already exist to avoid duplicates
+    if not User.query.first():
+       users = [
+            User(username="alice"),
+            User(username="bob")
+       ]
+       db.session.add_all(users)
+       db.session.commit()
+       return "Created users: alice and bob"  
+    else:
+        return "user already exists"                                                                                         
+   
+
+
+@app.route('/check-session', methods=['GET'])
+def check_session():
+    if "user_id"  in session:
+        user = User.query.filter_by(id=session['user_id']).first()
+        return jsonify({"user": user.username, "message": "Logged in",})
+    else:
+        return jsonify({
+            'message':  "Not logged in"
+        }), 401
+        
 
 if __name__  == '__main__':
   app.run(debug=True)
